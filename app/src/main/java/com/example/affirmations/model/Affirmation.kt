8ca6ad66c7ed0dc8c49749c682ee972c654c2c42
ngothanhphat <89116077+ngothanhphat@users.noreply.github.com>
2022-03-09 19:46:50 +0700

    Update

--- app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -1,4 +1,10 @@
 package com.example.affirmations.model
 
-data class Affirmation(val stringResourceId: Int) {
+import android.support.annotation.DrawableRes
+import android.support.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+) {
 }
